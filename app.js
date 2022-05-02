4145f67d241bf4d627293ba711682d4db89a5805
hernandez1981 <kathya_lucia@hotmail.com>
2022-05-02 16:49:38 -0600

    5. Agregando endpoint GET /v1/explorers

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`);
+    const explorer1 = {id: 1, name: "Katy"};
+    const explorer2 = {id: 2, name: "Porfirio"};
+    const explorer3 = {id: 3, name: "Coquillo"};
+    const explorer4 = {id: 4, name: "Nicolas"};
+    const explorers = [explorer1, explorer2, explorer3, explorer4];
+    res.status(200).json(explorers);
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
